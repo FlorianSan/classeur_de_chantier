674b7efc4478155638615f0135100b54362bff21
flo12 <fl.sansou@gmail.com>
2019-05-31 12:03:32 +0200

    Creation app

--- excel.py
+++
@@ -1,9 +0,0 @@
-from xlrd import open_workbook
-from xlutils.copy import copy
-
-rb = open_workbook("devis_t.xls")
-wb = copy(rb)
-
-s = wb.get_sheet(0)
-s.write(11,6,"test")
-wb.save("devis_t.xls")
